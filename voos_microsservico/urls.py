35258db46d5ab4d1c31befd5e4e4efd8c7335a4e
Thiago Chagas <tqchagas@gmail.com>
2018-11-03 13:51:37 -0300

    static

--- voos_microsservico/urls.py
+++ voos_microsservico/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from voos import views
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('aeroportos/', views.AeroportoListar.as_view()),
     path('voos/', views.VooCriarListar.as_view()),
     path('voos/<int:pk>/', views.VooDetalheApagar.as_view())
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
